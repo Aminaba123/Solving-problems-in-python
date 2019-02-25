34126d5ff1e0118e60d974f89ee3a3e50a91f022
Amin Abbasnejad <amin.abbasnejad@gmail.com>
2019-02-24 19:09:37 -0500

    Update Loops in Python.py

--- Loops in Python.py
+++ Loops in Python.py
@@ -5,3 +5,14 @@ while (count < 9):
 
 print "Good bye!"
 
+#######################################################
+
+var = 1
+while var == 1 :  # This constructs an infinite loop
+   num = raw_input("Enter a number  :")
+   print "You entered: ", num
+
+print "Good bye!"
+
+#######################################################
+
