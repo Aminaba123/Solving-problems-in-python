62d34eed64620a76e20d2ad9df1284931967bd55
Amin Abbasnejad <amin.abbasnejad@gmail.com>
2019-02-28 10:48:42 -0500

    Update Numpy.py

--- Numpy.py
+++ Numpy.py
@@ -1,3 +1,34 @@
 # style for reshaping
 
 scores_mean = np.array(scores_mean).reshape(len(grid_param_2),len(grid_param_1))
+
+##################################################################################
+
+# numpy to list
+
+a = np.array([1, 2])
+a.tolist()
+[1, 2]
+a = np.array([[1, 2], [3, 4]])
+list(a)
+[array([1, 2]), array([3, 4])]
+a.tolist()
+[[1, 2], [3, 4]]
+
+##################################################################################
+#1) Make an array of arrays:
+
+x=[[1,2],[1,2,3],[1]]
+y=numpy.array([numpy.array(xi) for xi in x])
+
+#2) Make an array of lists:
+
+x=[[1,2],[1,2,3],[1]]
+y=numpy.array(x)
+
+# As this is the top search on Google for converting a list of lists into a Numpy array,
+# I'll offer the following despite the question being 4 years old
+
+x = [[1, 2], [1, 2, 3], [1]]
+y = numpy.hstack(x)
+
