890c7cd63efcbd0af0e3ea89847e033c3ac8f30d
Amin Abbasnejad <amin.abbasnejad@gmail.com>
2019-02-28 13:07:53 -0500

    Update Loops in Python.py

--- Loops in Python.py
+++ Loops in Python.py
@@ -176,3 +176,41 @@ while n > 0: n -= 1; if True: print('foo')
 		
 #######################################################
 
+while len(sample) < n_sample:
+		index = randrange(len(dataset))
+		sample.append(dataset[index])
+		
+#######################################################
+
+ratio = 0.10
+for size in [1, 10, 100]:
+	sample_means = list()
+	for i in range(size):
+		sample = subsample(dataset, ratio)
+		sample_mean = mean([row[0] for row in sample])
+		sample_means.append(sample_mean)
+	print('Samples=%d, Estimated Mean: %.3f' % (size, mean(sample_means)))
+
+#######################################################
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
