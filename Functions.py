741569ac6188059cd60b0eb6fa55e11f56b1a859
Amin Abbasnejad <amin.abbasnejad@gmail.com>
2019-03-01 22:32:29 -0500

    Update Functions.py

--- Functions.py
+++ Functions.py
@@ -53,16 +53,41 @@ fibonacci()
 ###############################################################
 
 
+MY_DIR = r"c:\facedb\orl"
+# function for read in the first 20 images of ORL Dataset
+# orl1.bmp to orl10.bmp belong to one person, while orl11.bmp to orl20.bmp belong to another person.
+
+def get_training_data(myDir):
+    Training_data = []
+    i = 0
+    for img in os.listdir(myDir):
+        try:
+            img_array = cv2.imread(os.path.join(myDir,img) ,cv2.IMREAD_GRAYSCALE)
+            label = 0 if i<10 else 1
+            Training_data.append([img_array,label])
+            i = i+1
+        except Exception as e:
+            print('error')
+            break
+        if i >= 20:
+            break
+    return Training_data
 
 
+###############################################################
 
+def myfun(x):
+    if type(x) == str:
+        return len(str.split(x))
 
+###############################################################
 
+def solution1():
+	print(a)
+	N = len(a)
+	for i in range(N >> 1):
+		a[i],a[N-1-i]=a[N-1-i],a[i]
 
-
-
-
-
-
+###############################################################
 
 
