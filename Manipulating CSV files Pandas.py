58ca7c56c8030bd158391dd1ebc3900c98fbdf6e
Amin Abbasnejad <amin.abbasnejad@gmail.com>
2019-03-06 16:38:49 -0500

    Update Manipulating CSV files Pandas.py

--- Manipulating CSV files Pandas.py
+++ Manipulating CSV files Pandas.py
@@ -108,22 +108,236 @@ if abc  > string_date :
                  
 #################################################################################################                 
                  
+import csv
+with open('crop_data.csv', 'rb') as csvfile:
+    spamreader = csv.reader(csvfile, delimiter=' ', quotechar='|')
+    for row in spamreader:
+        print(', '.join(row))                 
                  
                  
+#Indiana,Corn,Genetically, engineered, (GE), corn,Stacked, gene, varieties,2012,Percent, of, all, corn, planted,60
+#Indiana,Corn,Genetically, engineered, (GE), corn,Stacked, gene, varieties,2013,Percent, of, all, corn, planted,73
+#Indiana,Corn,Genetically, engineered, (GE), corn,Stacked, gene, varieties,2014,Percent, of, all, corn, planted,78
+#Indiana,Corn,Genetically, engineered, (GE), corn,Stacked, gene, varieties,2015,Percent, of, all, corn, planted,76
+#Indiana,Corn,Genetically, engineered, (GE), corn,Stacked, gene, varieties,2016,Percent, of, all, corn, planted,75
+#Indiana,Corn,Genetically, engineered, (GE), corn,All, GE, varieties,2000,Percent, of, all, corn, planted,11
+#Indiana,Corn,Genetically, engineered, (GE), corn,All, GE, varieties,2001,Percent, of, all, corn, planted,12
+#Indiana,Corn,Genetically, engineered, (GE), corn,All, GE, varieties,2002,Percent, of, all, corn, planted,13
+#Indiana,Corn,Genetically, engineered, (GE), corn,All, GE, varieties,2003,Percent, of, all, corn, planted,16
+#Indiana,Corn,Genetically, engineered, (GE), corn,All, GE, varieties,2004,Percent, of, all, corn, planted,21
+#Indiana,Corn,Genetically, engineered, (GE), corn,All, GE, varieties,2005,Percent, of, all, corn, planted,26
+#Indiana,Corn,Genetically, engineered, (GE), corn,All, GE, varieties,2006,Percent, of, all, corn, planted,40
+#Indiana,Corn,Genetically, engineered, (GE), corn,All, GE, varieties,2007,Percent, of, all, corn, planted,59
+#Indiana,Corn,Genetically, engineered, (GE), corn,All, GE, varieties,2008,Percent, of, all, corn, planted,78
+#Indiana,Corn,Genetically, engineered, (GE), corn,All, GE, varieties,2009,Percent, of, all, corn, planted,79
+#Indiana,Corn,Genetically, engineered, (GE), corn,All, GE, varieties,2010,Percent, of, all, corn, planted,83
+#Indiana,Corn,Genetically, engineered, (GE), corn,All, GE, varieties,2011,Percent, of, all, corn, planted,85
+#Indiana,Corn,Genetically, engineered, (GE), corn,All, GE, varieties,2012,Percent, of, all, corn, planted,84
+#Indiana,Corn,Genetically, engineered, (GE), corn,All, GE, varieties,2013,Percent, of, all, corn, planted,85
+#Indiana,Corn,Genetically, engineered, (GE), corn,All, GE, varieties,2014,Percent, of, all, corn, planted,88
+#Indiana,Corn,Genetically, engineered, (GE), corn,All, GE, varieties,2015,Percent, of, all, corn, planted,88
+#Indiana,Corn,Genetically, engineered, (GE), corn,All, GE, varieties,2016,Percent, of, all, corn, planted,86                
+                 
+import csv
+with open('crop_data.csv', 'rb') as csvfile:
+    spamreader = csv.reader(csvfile, delimiter=' ', quotechar='|')
+    for row in spamreader:
+        print row                 
                  
                  
+#['Indiana,Corn,Genetically', 'engineered', '(GE)', 'corn,Stacked', 'gene', 'varieties,2012,Percent', 'of', 'all', 'corn', 'planted,60']
+#['Indiana,Corn,Genetically', 'engineered', '(GE)', 'corn,Stacked', 'gene', 'varieties,2013,Percent', 'of', 'all', 'corn', 'planted,73']
+#['Indiana,Corn,Genetically', 'engineered', '(GE)', 'corn,Stacked', 'gene', 'varieties,2014,Percent', 'of', 'all', 'corn', 'planted,78']
+#['Indiana,Corn,Genetically', 'engineered', '(GE)', 'corn,Stacked', 'gene', 'varieties,2015,Percent', 'of', 'all', 'corn', 'planted,76']
+#['Indiana,Corn,Genetically', 'engineered', '(GE)', 'corn,Stacked', 'gene', 'varieties,2016,Percent', 'of', 'all', 'corn', 'planted,75']
+#['Indiana,Corn,Genetically', 'engineered', '(GE)', 'corn,All', 'GE', 'varieties,2000,Percent', 'of', 'all', 'corn', 'planted,11']
+#['Indiana,Corn,Genetically', 'engineered', '(GE)', 'corn,All', 'GE', 'varieties,2001,Percent', 'of', 'all', 'corn', 'planted,12']
+#['Indiana,Corn,Genetically', 'engineered', '(GE)', 'corn,All', 'GE', 'varieties,2002,Percent', 'of', 'all', 'corn', 'planted,13']
+#['Indiana,Corn,Genetically', 'engineered', '(GE)', 'corn,All', 'GE', 'varieties,2003,Percent', 'of', 'all', 'corn', 'planted,16']
+#['Indiana,Corn,Genetically', 'engineered', '(GE)', 'corn,All', 'GE', 'varieties,2004,Percent', 'of', 'all', 'corn', 'planted,21']
+#['Indiana,Corn,Genetically', 'engineered', '(GE)', 'corn,All', 'GE', 'varieties,2005,Percent', 'of', 'all', 'corn', 'planted,26']
+#['Indiana,Corn,Genetically', 'engineered', '(GE)', 'corn,All', 'GE', 'varieties,2006,Percent', 'of', 'all', 'corn', 'planted,40']
+#['Indiana,Corn,Genetically', 'engineered', '(GE)', 'corn,All', 'GE', 'varieties,2007,Percent', 'of', 'all', 'corn', 'planted,59']
+#['Indiana,Corn,Genetically', 'engineered', '(GE)', 'corn,All', 'GE', 'varieties,2008,Percent', 'of', 'all', 'corn', 'planted,78']
+#['Indiana,Corn,Genetically', 'engineered', '(GE)', 'corn,All', 'GE', 'varieties,2009,Percent', 'of', 'all', 'corn', 'planted,79']
+#['Indiana,Corn,Genetically', 'engineered', '(GE)', 'corn,All', 'GE', 'varieties,2010,Percent', 'of', 'all', 'corn', 'planted,83']
+#['Indiana,Corn,Genetically', 'engineered', '(GE)', 'corn,All', 'GE', 'varieties,2011,Percent', 'of', 'all', 'corn', 'planted,85']
+#['Indiana,Corn,Genetically', 'engineered', '(GE)', 'corn,All', 'GE', 'varieties,2012,Percent', 'of', 'all', 'corn', 'planted,84']
+#['Indiana,Corn,Genetically', 'engineered', '(GE)', 'corn,All', 'GE', 'varieties,2013,Percent', 'of', 'all', 'corn', 'planted,85']
+#['Indiana,Corn,Genetically', 'engineered', '(GE)', 'corn,All', 'GE', 'varieties,2014,Percent', 'of', 'all', 'corn', 'planted,88']
+#['Indiana,Corn,Genetically', 'engineered', '(GE)', 'corn,All', 'GE', 'varieties,2015,Percent', 'of', 'all', 'corn', 'planted,88']
+#['Indiana,Corn,Genetically', 'engineered', '(GE)', 'corn,All', 'GE', 'varieties,2016,Percent', 'of', 'all', 'corn', 'planted,86']                 
+
+Hints:                 
+# The csv module’s reader and writer objects read and write sequences.
+# Programmers can also read and write data in dictionary form using the 'DictReader' and 'DictWriter' classes.                 
+
+#####################################################################################################################
+                 
+def convertLines(lines):
+    head = lines[0]
+    del lines[0]
+    infoDict = {}
+    for line in lines: #Going through everything but the first line
+        infoDict[line.split(",")[0]] = [tuple(line.split(",")[1:])]
+    return infoDict
+
+def read_file(filename):
+    thefile = open(filename, "r")
+    lines = []
+    for i in thefile:
+        lines.append(i)
+    thefile.close()
+    return lines
+
+mydict = convertLines(read_file(filename))
+print(mydict)                 
+                 
+#####################################################################################################################
+                 
+import csv
+from collections import defaultdict
+
+d_dict = defaultdict(list)
+with open('file.txt') as f:
+    reader = csv.reader(f)
+    reader.next()
+    for i in list(reader):
+        d_dict[i[0]].append(tuple(i[1:]))
+
+print dict(d_dict)                
+                 
+#####################################################################################################################                 
+
+import csv
+reader = csv.reader(open('CamCloseM6 4V300E10.csv'))
+
+result = {}
+for column in reader:
+    key = column[0]
+    if key in result:
+        pass
+    result[key] = column[1:]
+print result
+                 
+#####################################################################################################################                 
+                 
+import csv
+reader = csv.reader(open('CamCloseM6 4V300E10.csv'))
+
+result = {row[0]:row[1:] for row in reader if row and row[0]}
+print result                 
                  
                  
+ #####################################################################################
+                
+ import csv
+reader = csv.reader(open('test.csv'))
+
+result = {row[0]:[i for i in row[1:] if i] for row in reader if row and row[0]}
+print result                
                  
+#####################################################################################                
                  
+import csv
+from collections import OrderedDict
+result = OrderedDict()
+with open('CamCloseM6 4V300E10.csv') as f:
+    for row  in f:
+        row=row.strip().split()
+        key = row[0]
+        result[key] = row[1:]
+print result               
                  
+#####################################################################################                
+
+import csv
+from collections import OrderedDict
+result = OrderedDict()
+with open('crop_data.csv') as f:
+    for row  in f:
+        row=row.strip().split()
+        print row                 
                  
+#####################################################################################                
+
+# You can use a defaultdict to store the values and then print them out:
                  
+import csv
+
+from collections import defaultdict
+
+with open(filename, 'r') as handle:
+    reader = csv.DictReader(handle, ['name', 'miles', 'country'])
+    data = defaultdict(list)
+
+    for line in reader:
+        data[line['name']).append(int(line['miles']))
+
+    for runner, distances in data.items():
+        print '{} ran a total of {} miles and an average of {} miles'.format(
+            runner, sum(distances), sum(distances) / float(len(distances))
+        )                
                  
+#####################################################################################                 
+import csv
+
+from collections import defaultdict
+
+filename = 'new-1.csv'
+
+with open(filename, 'r') as handle:
+    reader = csv.DictReader(handle)
+    for line in reader:
+        print line                 
                  
-                 
-                 
-                 
-                 
-                 
-                 
+#####################################################################################               
+
+import csv
+
+from collections import defaultdict
+
+with open(filename, 'r') as handle:
+    reader = csv.DictReader(handle, ['name', 'miles', 'country'])
+    data = defaultdict(list)
+
+    for line in reader:
+        data[line['name']).append(int(line['miles']))
+
+    for runner, distances in data.items():
+        print '{} ran a total of {} miles and an average of {} miles'.format(
+            runner, sum(distances), sum(distances) / float(len(distances))
+        )
+             
+#####################################################################################
+# This returns list of ...             
+varieties = []
+with open('new-1.csv', 'r') as infile:
+    for line in infile:
+        #if "All GE varieties" in line:
+        varieties.append(line.split(','))            
+             
+             
+ #####################################################################################            
+  # This returns list of ... but it contains string 
+             
+varieties = []
+with open('new-1.csv', 'r') as infile:
+    for line in infile:
+        #if "All GE varieties" in line:
+        varieties.append(line)           
+           
+             
+ #####################################################################################            
+             
+             
+             
+             
+             
+             
+             
+             
+             
+             
+             
+             
