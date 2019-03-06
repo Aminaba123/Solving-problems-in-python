7d07ec04adcf752bf94b7c494b30a58699839d96
Amin Abbasnejad <amin.abbasnejad@gmail.com>
2019-03-06 16:38:46 -0500

    Update Working with Dictionaries.py

--- Working with Dictionaries.py
+++ Working with Dictionaries.py
@@ -36,6 +36,78 @@ for key in d:
 print sampleDict.values()[0].keys()[0]	
 
 ###########################################################
+# Check if a given key already exists in a dictionary
+# in is the intended way to test for the existence of a key in a dict
+d = dict()
+
+for i in xrange(100):
+    key = i % 10
+    if key in d:
+        d[key] += 1
+    else:
+        d[key] = 1
+
+# If you wanted a default, you can always use dict.get():
+
+d = dict()
+
+for i in xrange(100):
+    key = i % 10
+    d[key] = d.get(key, 0) + 1
+
+###############################	#############################
+
+if 'key1' in dict:
+  print "blah"
+else:
+  print "boo"
+
+##########################################################
+
+import csv
+reader = csv.reader(open('CamCloseM6 4V300E10.csv'))
+
+result = {}
+for column in reader:
+    key = column[0]
+    if key in result:
+        pass
+    result[key] = column[1:]
+print result
+
+##########################################################
+
+import csv
+reader = csv.reader(open('CamCloseM6 4V300E10.csv'))
+
+result = {row[0]:row[1:] for row in reader if row and row[0]}
+print result
+
+##########################################################
+
+# How do I sort a list of dictionaries by a value of the dictionary?
+
+newlist = sorted(list_to_be_sorted, key=lambda k: k['name']) 
+
+import operator
+# To sort the list of dictionaries by key='name':
+
+list_of_dicts.sort(key=operator.itemgetter('name'))
+# To sort the list of dictionaries by key='age':
+
+list_of_dicts.sort(key=operator.itemgetter('age'))
+
+##########################################################
+
+
+
+
+
+
+
+
+
+
 
 
 
