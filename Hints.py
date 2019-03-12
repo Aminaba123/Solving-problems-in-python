8d0859f1fe363ac82255f91e42580d104a803c55
Amin Abbasnejad <amin.abbasnejad@gmail.com>
2019-03-12 19:45:28 -0400

    Create Hints.py

---
+++ Hints.py
@@ -0,0 +1,8 @@
+###########
+the equality operator ==, as in 'a' == 'a'.
+
+#  If the two single characters are the same, the expression returns True
+#  Note that the expression 'a' == 'A' returns False as those are indeed two different strings.
+
+'a' == 'A'   # returns False as those are indeed two different strings.
+
