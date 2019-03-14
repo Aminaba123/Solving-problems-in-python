19b1eb3009c85c40eb48297ac267dfb827355bbf
Amin Abbasnejad <amin.abbasnejad@gmail.com>
2019-03-14 15:34:42 -0400

    Update Manipulating CSV files Pandas.py

--- Manipulating CSV files Pandas.py
+++ Manipulating CSV files Pandas.py
@@ -412,12 +412,71 @@ def addDateColumn():
         #append each output to same csv, using no header
         chunk.to_csv(pathlist[2]+outname, mode='a', header=None, index=None)             
              
+ #####################################################################################            
+# Example CSV file data:             
              
+# 1/2/2014,5,8,red
+# 1/3/2014,5,2,green
+# 1/4/2014,9,1,blue            
              
+# Next, let's cover the reading of CSV files into memory:
+
+import csv
+
+with open('example.csv') as csvfile:
+    readCSV = csv.reader(csvfile, delimiter=',')
+    for row in readCSV:
+        print(row)
+        print(row[0])
+        print(row[0],row[1],row[2],)             
              
+ # Next, we will show how to pull out specific data from the spreadsheet and save it to a list variable:
+
+ import csv
+
+with open('example.csv') as csvfile:
+    readCSV = csv.reader(csvfile, delimiter=',')
+    dates = []
+    colors = []
+    for row in readCSV:
+        color = row[3]
+        date = row[0]
+
+        dates.append(date)
+        colors.append(color)
+
+    print(dates)
+    print(colors)           
              
-             
-             
-             
-             
-             
+# Once we have this data, what can we do with it? Maybe we are curious about what color something was on a specific date.
+
+import csv
+
+with open('example.csv') as csvfile:
+    readCSV = csv.reader(csvfile, delimiter=',')
+    dates = []
+    colors = []
+    for row in readCSV:
+        color = row[3]
+        date = row[0]
+
+        dates.append(date)
+        colors.append(color)
+
+    print(dates)
+    print(colors)
+
+    # now, remember our lists?
+
+    whatColor = input('What color do you wish to know the date of?:')
+    coldex = colors.index(whatColor)
+    theDate = dates[coldex]
+    print('The date of',whatColor,'is:',theDate)
+              
+##################################################################################
+              
+              
+              
+              
+              
+              
