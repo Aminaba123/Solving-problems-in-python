810340555ab5921b3ff0d3d4bc82893f2d8d9bb7
Amin Abbasnejad <amin.abbasnejad@gmail.com>
2019-03-14 15:31:40 -0400

    Create Defining window.py

---
+++ Defining window.py
@@ -0,0 +1,77 @@
+import scipy, pylab
+
+def stft(x, fs, framesz, hop):
+    framesamp = int(framesz*fs)
+    hopsamp = int(hop*fs)
+    w = scipy.hanning(framesamp)
+    X = scipy.array([scipy.fft(w*x[i:i+framesamp]) 
+                     for i in range(0, len(x)-framesamp, hopsamp)])
+    return X
+
+def istft(X, fs, T, hop):
+    x = scipy.zeros(T*fs)
+    framesamp = X.shape[1]
+    hopsamp = int(hop*fs)
+    for n,i in enumerate(range(0, len(x)-framesamp, hopsamp)):
+        x[i:i+framesamp] += scipy.real(scipy.ifft(X[n]))
+    return x
+#############################################################################################
+
+import scipy, numpy as np
+
+def stft(x, fftsize=1024, overlap=4):   
+    hop = fftsize / overlap
+    w = scipy.hanning(fftsize+1)[:-1]      # better reconstruction with this trick +1)[:-1]  
+    return np.array([np.fft.rfft(w*x[i:i+fftsize]) for i in range(0, len(x)-fftsize, hop)])
+
+def istft(X, overlap=4):   
+    fftsize=(X.shape[1]-1)*2
+    hop = fftsize / overlap
+    w = scipy.hanning(fftsize+1)[:-1]
+    x = scipy.zeros(X.shape[0]*hop)
+    wsum = scipy.zeros(X.shape[0]*hop) 
+    for n,i in enumerate(range(0, len(x)-fftsize, hop)): 
+        x[i:i+fftsize] += scipy.real(np.fft.irfft(X[n])) * w   # overlap-add
+        wsum[i:i+fftsize] += w ** 2.
+    pos = wsum != 0
+    x[pos] /= wsum[pos]
+    return x
+    
+ #############################################################################################
+
+import scipy, pylab
+import numpy as np
+
+def stft(x, fs, framesz, hop):
+    """
+     x - signal
+     fs - sample rate
+     framesz - frame size
+     hop - hop size (frame size = overlap + hop size)
+    """
+    framesamp = int(framesz*fs)
+    hopsamp = int(hop*fs)
+    w = scipy.hamming(framesamp)
+    X = scipy.array([scipy.fft(w*x[i:i+framesamp]) 
+                     for i in range(0, len(x)-framesamp, hopsamp)])
+    return X
+
+def istft(X, fs, T, hop):
+    """ T - signal length """
+    length = T*fs
+    x = scipy.zeros(T*fs)
+    framesamp = X.shape[1]
+    hopsamp = int(hop*fs)
+    for n,i in enumerate(range(0, len(x)-framesamp, hopsamp)):
+        x[i:i+framesamp] += scipy.real(scipy.ifft(X[n]))
+    # calculate the inverse envelope to scale results at the ends.
+    env = scipy.zeros(T*fs)
+    w = scipy.hamming(framesamp)
+    for i in range(0, len(x)-framesamp, hopsamp):
+        env[i:i+framesamp] += w
+    env[-(length%hopsamp):] += w[-(length%hopsamp):]
+    env = np.maximum(env, .01)
+    return x/env
+    
+#############################################################################################
+
