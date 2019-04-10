4b54b43d81d558591e033ba781a29afaa57a00d6
Amin Abbasnejad <amin.abbasnejad@gmail.com>
2019-04-10 11:41:41 -0400

    Update Working with directory.py

--- Working with directory.py
+++ Working with directory.py
@@ -242,9 +242,23 @@ for root, subdirs, files in os.walk(walk_dir):
 
 ######################################################################
 
+import os
 
+path = "/usr/tmp"
 
+# Check current working directory.
+retval = os.getcwd()
+print "Current working directory %s" % retval
 
+# Now change the directory
+os.chdir( path )
+
+# Check current working directory.
+retval = os.getcwd()
+
+print "Directory changed successfully %s" % retval
+
+######################################################################
 
 
 
