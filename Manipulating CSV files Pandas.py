f0bd1567897b86fcd97ae4c0e97079883609ad43
Amin Abbasnejad <amin.abbasnejad@gmail.com>
2019-05-15 12:33:56 -0400

    Update Manipulating CSV files Pandas.py

--- Manipulating CSV files Pandas.py
+++ Manipulating CSV files Pandas.py
@@ -480,8 +480,23 @@ for i in range(1,100):
     df['X_t'+str(i)] = df['X'].shift(i)
 
 ###############################################################              
+
+dic = {"John": "john@example.com", "Mary": "mary@example.com"} #dictionary
+
+download_dir = "exampleCsv.csv" #where you want the file to be downloaded to 
+
+csv = open(download_dir, "w") 
+#"w" indicates that you're writing strings to the file
+
+columnTitleRow = "name, email\n"
+csv.write(columnTitleRow)
+
+for key in dic.keys():
+	name = key
+	email = dic[key]
+	row = name + "," + email + "\n"
+	csv.write(row)              
               
               
-              
-              
+ ###############################################################             
               
