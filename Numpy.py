0c109e9139701343eb3a326e3472e4a69fd6f4de
Amin Abbasnejad <amin.abbasnejad@gmail.com>
2019-05-16 11:13:08 -0400

    Update Numpy.py

--- Numpy.py
+++ Numpy.py
@@ -66,3 +66,35 @@ new_x = np.delete(x, index)
 print(new_x)
 
 ##################################################################################
+
+# In order to create the concatenation and work around the error, I initialized the array with None and 
+# tested if it is None in the loop. Thereby you do not have to worry about not fitting dimensions. 
+# However, i created some arrays for the ones you did only describe and ended up with a final 
+# dimesion of (400, 30, 30, 3). This fits in here, since 20*20 = 400. Hope this helps for you 
+new_one_arr1_list = []
+new_one_arr2_list = []
+one_arr1 = np.ones((20,30,30,3))
+one_arr2 = np.ones((20,30,30,3))
+all_arr1 = None
+count = 0
+for item in one_arr1: # 100 iterations
+    item = np.reshape(item, (1, 30, 30, 3))
+    new_one_arr1 = np.repeat(item, 20, axis=0)
+
+    # print(all_arr1.shape, new_one_arr1.shape)
+    if all_arr1 is None:
+        all_arr1 = new_one_arr1
+    else:
+        all_arr1 = np.concatenate(([all_arr1 , new_one_arr1 ]), axis=0)
+    ind = np.random.randint(one_arr2.shape[0], size=(20,))
+    new_one_arr2= one_arr1[ind]
+
+    new_one_arr1_list.append(new_one_arr1)
+    new_one_arr2_list.append(new_one_arr2)
+    count += 1
+print(count)
+all_arr1.shape
+
+##################################################################################à
+
+
