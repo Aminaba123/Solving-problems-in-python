b4e99a095f9751cf2032c9c898d30d9d82f90506
Amin Abbasnejad <amin.abbasnejad@gmail.com>
2019-05-29 12:50:57 -0400

    Update Hints.py

--- Hints.py
+++ Hints.py
@@ -11,3 +11,22 @@ to a n int if we wish to do atithmatic on it
 
 # we need varibale to hold our count of pairs , and we can initialiseze it to 0
 
+##################################################################
+# these are not the same we should change the place of return
+def x_gen (x):
+    lo=[]
+    for i in range(len(x)):
+        lo.append(x[:i])
+    return lo
+  
+def x_gen (x):
+    lo=[]
+    for i in range(len(x)):
+        lo.append(x[:i])
+        return lo  
+      
+      
+####################################################################### 
+
+
+
